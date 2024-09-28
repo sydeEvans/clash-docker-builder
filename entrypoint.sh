ef80701e1d2981662328d3c38556514aeae302ea
jax <evan.wangjue@gmail.com>
2024-09-29 00:14:01 +0800

    允许局域网

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-curl -o /home/runner/.config/clash/clash.yaml "$SUB_URL"
+curl -o /home/runner/.config/clash/config.yaml "$SUB_URL"
+
+# 允许局域网链接
+sed -i 's/allow-lan: false/allow-lan: true/g' /home/runner/.config/clash/config.yaml
 
 exec /usr/local/bin/entrypoint.sh "$@"
